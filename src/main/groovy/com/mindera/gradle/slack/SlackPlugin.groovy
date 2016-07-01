ecb62b1a5589636fdca8ab0a51a867ce2b91cc2d
Pete Stringer <peter.stringer@degree53.com>
2016-07-01 14:03:31 +0100

    Reversed previous change. Wasn't thinking straight!

--- src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
+++ src/main/groovy/com/mindera/gradle/slack/SlackPlugin.groovy
@@ -52,7 +52,7 @@ class SlackPlugin implements Plugin<Project> {
 
     void handleTaskFinished(Task task, TaskState state) {
         Throwable failure = state.getFailure()
-        boolean shouldSendMessage = true; //failure != null || shouldMonitorTask(task);
+        boolean shouldSendMessage = failure != null || shouldMonitorTask(task);
 
         // only send a slack message if the task failed
         // or the task is registered to be monitored
